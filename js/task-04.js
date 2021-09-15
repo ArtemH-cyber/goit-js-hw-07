17edb7ddc592bde071c148784cb06f8f79edb2b0
Artem Halchyk <art234m@gmail.com>
2021-09-15 11:03:05 +0300

    Update task-04.js

--- js/task-04.js
+++ js/task-04.js
@@ -1,51 +1,52 @@
 
-const refs = {
-    buttonDecrement: document.querySelector("[data-action='decrement']"),
-    buttonIncrement: document.querySelector("[data-action='increment']"),
-    counterValue: document.getElementById("value"),
-}
 
-refs.buttonDecrement.addEventListener('click', () => {
-     
-    let numberValue = Number(refs.counterValue.textContent)
-    refs.counterValue.textContent = numberValue - 1      
-})
+const counterValue = document.querySelector("#value");
+const decrementRef = document.querySelector('[data-action="decrement"]');
+const incrementRef = document.querySelector('[data-action="increment"]');
 
+let valueRef = 0;
 
-refs.buttonIncrement.addEventListener('click', () => { 
-    
-    let numberValue = Number(refs.counterValue.textContent)
-    refs.counterValue.textContent = numberValue + 1  
-})
+const changeValue = () => {
+  counterValue.textContent = valueRef;
+};
+
+decrementRef.addEventListener("click", () => {
+  decrement();
+  changeValue();
+});
+
+incrementRef.addEventListener("click", () => {
+  increment();
+  changeValue();
+});
+
+const decrement = () => {
+  valueRef -= 1;
+};
+
+const increment = () => {
+  valueRef += 1;
+};
 
 
 
 
 // способ 2
-// const counterValue = document.querySelector("#value");
-// const decrementRef = document.querySelector('[data-action="decrement"]');
-// const incrementRef = document.querySelector('[data-action="increment"]');
+// const refs = {
+//     buttonDecrement: document.querySelector("[data-action='decrement']"),
+//     buttonIncrement: document.querySelector("[data-action='increment']"),
+//     counterValue: document.getElementById("value"),
+// }
 
-// let valueRef = 0;
+// refs.buttonDecrement.addEventListener('click', () => {
+     
+//     let numberValue = Number(refs.counterValue.textContent)
+//     refs.counterValue.textContent = numberValue - 1      
+// })
 
-// const changeValue = () => {
-//   counterValue.textContent = valueRef;
-// };
 
-// decrementRef.addEventListener("click", () => {
-//   decrement();
-//   changeValue();
-// });
-
-// incrementRef.addEventListener("click", () => {
-//   increment();
-//   changeValue();
-// });
-
-// const decrement = () => {
-//   valueRef -= 1;
-// };
-
-// const increment = () => {
-//   valueRef += 1;
-// };
+// refs.buttonIncrement.addEventListener('click', () => { 
+    
+//     let numberValue = Number(refs.counterValue.textContent)
+//     refs.counterValue.textContent = numberValue + 1  
+// })
